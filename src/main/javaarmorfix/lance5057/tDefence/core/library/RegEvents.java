ae6860bc72e5577f298a3201e1c6536d60b10b75
xemnes <xemnes@live.co.uk>
2021-03-02 18:05:17 +0000

    Delete src/main/javaarmorfix/lance5057/tDefence directory

--- src/main/javaarmorfix/lance5057/tDefence/core/library/RegEvents.java
+++
@@ -1,5 +0,0 @@
-package lance5057.tDefence.core.library;
-
-public class RegEvents {
-	
-}
--- src/main/javaarmorfix/lance5057/tDefence/core/materials/traits/TraitCoolHead.java
+++
@@ -1,5 +0,0 @@
-package lance5057.tDefence.core.materials.traits;
-
-public class TraitCoolHead extends AbstractTDTrait {
-
-}
